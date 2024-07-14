76e7139688c6d9ed47031b3d84ce3b13583243b7
sumeetshinde <sumeetshinde111@gmail.com>
2024-07-14 22:20:26 +0530

    Add mmain component to todo

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import { FormInput } from './Components/FormInput';
+import { Todo } from './Components/Todo';
 
 function App() {
   return (
     <div className="App">
-     <h1>Todo List</h1>
-     <FormInput/>
+    <Todo/>
     </div>
   );
 }
---
+++ todo/src/Components/Todo.jsx
@@ -0,0 +1,11 @@
+import { FormInput } from "./FormInput"
+
+export const Todo = ()=>{
+    return(
+        <>
+            <h1>Todo</h1>
+            <FormInput/>
+        
+        </>
+    )
+}
