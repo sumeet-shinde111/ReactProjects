3f44cb3d3f757e98455fd8a8bcbbc0f22af35d17
sumeetshinde <sumeetshinde111@gmail.com>
2024-07-14 21:48:17 +0530

    Create input component for todolist

---
+++ todo/src/Components/FormInput.jsx
@@ -0,0 +1,11 @@
+
+export const FormInput  = ()=>{
+    return(
+        <>
+        <form >
+            <input type="text" placeholder = "Enter Todo item" />
+            <button type="submit">Add item</button>
+        </form>
+        </>
+    )
+}
